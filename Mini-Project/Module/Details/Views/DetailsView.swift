bca1166da6f8901f28b5750d7bc6616e866e37a5
Agestia Deanara Tori <agestia.dtori@gmail.com>
2024-12-09 12:38:19 +0700

    Initial Commit

---
+++ Mini-Project/Module/Details/Views/DetailsView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text("Halo Dunia!")
+    }
+}
+
+struct DetailsPreviews: PreviewProvider {
+    static var previews: some View {
+        DetailsView()
+    }
+}
